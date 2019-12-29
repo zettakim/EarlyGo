d4027bc66283737ee8beb92e0c834f755396966e
Zetta Kim <zettabyte.kim@gmail.com>
2019-12-29 14:05:47 +0900

    second commit

--- unit8.7.overflow/max-value.go
+++ unit8.7.overflow/max-value.go
@@ -10,7 +10,8 @@ func main() {
 	var num2 uint16 = math.MaxUint16
 	var num3 uint32 = math.MaxUint32
 	var num4 uint64 = math.MaxUint64
-	var num5 uint8 = math.MaxUint8 + 1
+	// var num5 uint8 = math.MaxUint8 + 1 // 오버플로우
+	// var num1 uint8 = 0 - 1  // 오버플로우 컴파일 에러 발생
 
 	fmt.Println(num1)
 	fmt.Println(num2)
