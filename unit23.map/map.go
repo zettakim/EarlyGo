65148a4a195dc151e64b6d733eada054c86a100b
Zetta Kim <zettabyte.kim@gmail.com>
2020-01-01 22:03:35 +0900

    1/1 final commit

--- unit23.map/map.go
+++ unit23.map/map.go
@@ -40,4 +40,41 @@ func main() {
 	if value, ok := solarSystem["Neptune"]; ok {
 		fmt.Println(value)
 	}
+
+	for key, value := range solarSystem {
+		fmt.Println(key, value)
+	}
+
+	for _, value := range solarSystem {
+		fmt.Println(value)
+	}
+
+	z := map[string]int{"hello": 10, "world": 20}
+	delete(z, "world")
+	fmt.Println(z)
+
+	terrestrialPlanet := map[string]map[string]float32{
+		"Mercury": map[string]float32{
+			"meanRadius":    2439.7,
+			"mass":          3.3022E+23,
+			"orbitalPeriod": 87.969,
+		},
+		"Venus": map[string]float32{
+			"meanRadius":    6051.8,
+			"mass":          4.8676E+24,
+			"orbitalPeriod": 224.70069,
+		},
+		"Earth": map[string]float32{
+			"meanRadius":    6371.0,
+			"mass":          5.97219E+24,
+			"orbitalPeriod": 365.25641,
+		},
+		"Mars": map[string]float32{
+			"meanRadius":    3389.5,
+			"mass":          6.4185E+23,
+			"orbitalPeriod": 686.9600,
+		},
+	}
+
+	fmt.Println(terrestrialPlanet["Mars"]["mass"])
 }
