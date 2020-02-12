e2d0c58f749aa1e48d1a07993fab7eba2477856e
Zetta Kim <zettabyte.kim@gmail.com>
2020-02-12 09:47:51 +0900

    sync commit

---
+++ unit35/unit35.1_동기화객체/synchronization.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var data = []int{}
+	var mutex = new(sync.Mutex)
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			mutex.Lock()
+			data = append(data, 1)
+			mutex.Unlock()
+
+			runtime.Gosched()
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			mutex.Lock()
+			data = append(data, 1)
+			mutex.Unlock()
+
+			runtime.Gosched()
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println(len(data))
+}
